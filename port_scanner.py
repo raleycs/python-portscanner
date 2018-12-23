3fd726da99e6271fc92e1a2aef2c7c8920107ec4
Christopher Raley <cr2fv@virginia.edu>
2018-12-23 09:02:41 -0500

    Version 1.0

--- port_scanner.py
+++ port_scanner.py
@@ -1,33 +1,87 @@
-# Python implementation of a port scanner
-import optparse
+#!/usr/bin/env python
 
-def pScan(host, ports):
-    # Implement actual scanning process
-    print(host)
-    for p in ports:
-        print(p)
+# Python Port Scanner
 
+import socket, subprocess, sys
+from datetime import datetime
 
-def main():
-    # Instantiate parser object with two parameters: tgtHost and tgtPort
-    parser = optparse.OptionParser(('usage %prog -H <target> -p <port(s) separated by a space>'))
-    parser.add_option("-H", dest="tgtHost", type="string", help="specify target Host")
-    parser.add_option("-p", dest="tgtPort", type="string", help="specify target port(s) separated by a space")
+# Clears the screen
+subprocess.call('clear',shell=True)
 
-    # Receive user parameters and store them as tuple
-    (options,args) = parser.parse_args()
+# Ask for remote host to scan
+target = input('Target: ')
 
-    # Check for improper user inputs
-    if (not options.tgtHost) | (not options.tgtPort):
-        print(parser.usage)
-        exit(0)
+# Translate domain to IPv4 address
+target_ip = socket.gethostbyname(target)
+print('Target\'s IP: ' + str(target_ip))
 
-    # Clean user input by stripping white space
-    tgtHost = str(options.tgtHost).strip()
-    # Strip white spaces and store ports in list
-    tgtPorts = [s.strip() for s in str(options.tgtPort).split(' ')]
+# Option for single port scan
+single = input('Input specific port or use \'default\' [default = 1 - 1024]: ')
 
-    pScan(tgtHost, tgtPorts)
+while(single == ''):
+    single = input('Please input a valid choice (default or port number): ')
 
-if __name__=="__main__":
-    main()
+# Separate information with dashes
+print('-' * 50)
+print('Scanning ' + str(target_ip))
+print('-' * 50)
+
+# Counter
+count = 0
+
+# Begin timing the scan
+t_initial = datetime.now()
+
+# Scan ports from 1-1024 or specific port
+# Use exceptions for errors
+
+try:
+    if single != 'default':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        fail = sock.connect_ex((target_ip, int(single)))
+
+        if fail == False:
+            print('Port ' + single + ': Open')
+            count += 1
+        else:
+            print('Port ' + single + ': Closed')
+        
+        sock.close()
+
+    else:
+        for port in range(1,1025):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            fail = sock.connect_ex((target_ip,port))
+            
+            if fail == False:
+                count += 1
+                print('Port ' + str(port) + ':  Open')
+                
+            sock.close()
+
+except socket.error:
+    print('Error: Unable to connect to target')
+    sys.exit()
+
+except socket.gaierror:
+    print('Error: Target cannot be resolved')
+    sys.exit()
+
+except KeyboardInterrupt:
+    print('Alert: Keyboard interrupt detected... exiting program')
+    sys.exit()
+
+# End timing
+t_end = datetime.now()
+
+# Total time
+t_result = t_end - t_initial
+
+print('-' * 50)
+
+# Analysis
+print('Scanning Complete')
+print('Target: ' + target)
+print('IP: ' + str(target_ip))
+print('Time Elapsed: ' + str(t_result))
+print('Number of ports open: ' + str(count))
